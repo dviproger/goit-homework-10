b92b5206659809c7b5f877b0ff102583fee2a4cc
dviproger <126821493+dviproger@users.noreply.github.com>
2023-06-11 23:45:45 +0300

    Create Home-work10.py

---
+++ Home-work10.py
@@ -0,0 +1,51 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+
+    def add_record(self, record):
+        self.data.update({record.name: record.phones})
+        
+    def search(self):
+        pass
+
+class Record():
+    def __init__(self, name, phone):
+        self.name = name
+        self.phones = [phone]
+        
+    def get_record(self):
+        print(f'{self.name} {self.phones}')
+        return {self.name.value: self.phones}
+
+    def add_phone(self, phone):
+        self.phones.append(phone)
+
+        
+    def edit_phone(self, old_phone, new_phone):
+        index_old_phone = self.phones.index(old_phone)
+        self.phones[index_old_phone] = new_phone
+        
+    def delete(self, phone):
+        self.phones.remove(phone)
+
+
+class Field():
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+    
+name = Name('kirk')
+phone = Phone('09923423423')
+record = Record(name, phone)
+print(record.name.value)
+
+print(record.get_record())
